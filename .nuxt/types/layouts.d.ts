265621d1fbc5adaaf6af87bf0a32a6d560f564b8
Luis Felipe Gomez Giraldo <100101763+PipeGom@users.noreply.github.com>
2023-09-15 20:18:01 -0500

    Delete .nuxt directory

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "blank" | "default"
-declare module "C:/Users/luisf/Desktop/Universidad/Materias/semestre 6/Desarrollo web/dllo-web/Actividad-3 login vercel/AppTask/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "C:/Users/luisf/Desktop/Universidad/Materias/semestre 6/Desarrollo web/dllo-web/Actividad-3 login vercel/AppTask/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
